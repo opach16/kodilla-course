73ff455da995bedefbd61ec121004288719832a5
Konrad Opach <opach16@outlook.com>
2024-09-08 10:50:57 +0200

    14.2 Prototype - Library Clone

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -0,0 +1,40 @@
+package com.kodilla.patterns.prototype.library;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class Library implements Cloneable {
+
+    private final String name;
+    private Set<Book> books = new HashSet<>();
+
+    public Library(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public Library shallowClone() throws CloneNotSupportedException {
+        return (Library) super.clone();
+    }
+
+    public Library deepClone() throws CloneNotSupportedException {
+        Library clonedLibrary = (Library) super.clone();
+        clonedLibrary.books = new HashSet<>();
+        for (Book book : books) {
+            clonedLibrary.getBooks().add(book);
+        }
+        return clonedLibrary;
+    }
+
+    @Override
+    public String toString() {
+        return name + "\n" + books;
+    }
+}
---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -0,0 +1,46 @@
+package com.kodilla.patterns.prototype.library;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LibraryTestSuite {
+
+    @Test
+    void testGetBooks() {
+        //given
+        Library library = new Library("National Library");
+        Book book = new Book("Title", "Author", LocalDate.now());
+        library.getBooks().add(book);
+        for (int i = 1; i < 10; i++) {
+            library.getBooks().add(new Book("Title" + i, "Author" + (i + 100), LocalDate.now().minusYears(i)));
+        }
+
+        Library shallowClonedLibrary = null;
+        try {
+            shallowClonedLibrary = library.shallowClone();
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Library deepClonedLibrary = null;
+        try {
+            deepClonedLibrary = library.deepClone();
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //when
+        library.getBooks().remove(book);
+
+        //then
+        System.out.println(library);
+        System.out.println(shallowClonedLibrary);
+        System.out.println(deepClonedLibrary);
+        assertEquals(9, library.getBooks().size());
+        assertEquals(9, shallowClonedLibrary.getBooks().size());
+        assertEquals(10, deepClonedLibrary.getBooks().size());
+    }
+}
