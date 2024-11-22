ba2ea0de541f3330297c16d5f6fa02dde983d697
Konrad Opach <opach16@outlook.com>
2024-11-22 12:09:42 +0100

    23.1 Facade

---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/facade/ShopServiceTestSuite.java
@@ -0,0 +1,34 @@
+package com.kodilla.patterns2.facade;
+
+import com.kodilla.patterns2.facade.api.ItemDto;
+import com.kodilla.patterns2.facade.api.OrderDto;
+import com.kodilla.patterns2.facade.api.OrderFacade;
+import com.kodilla.patterns2.facade.api.OrderProcessingException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ShopServiceTestSuite {
+
+    @Autowired
+    private OrderFacade orderFacade;
+
+    @Autowired
+    private ShopService shopService;
+
+    @Test
+    void testShopFacade() {
+        OrderDto order = new OrderDto();
+        order.addItem(new ItemDto(10L, 2));
+        order.addItem(new ItemDto(216L, 1));
+        order.addItem(new ItemDto(25L, 1));
+        order.addItem(new ItemDto(11L, 3));
+
+        try {
+            orderFacade.processOrder(order, 1L);
+        } catch (OrderProcessingException e) {
+            // nothing here
+        }
+    }
+}
