bb0a1bab0581d464259662fd04d8b2caa7244098
Konrad Opach <opach16@outlook.com>
2024-12-03 11:30:56 +0100

    28.3 Test for UpdateBestsellers method

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -33,4 +33,28 @@ public class StoredProcTestSuite {
         statement.close();
         statement2.close();
     }
+
+    @Test
+    void testUpdateBestsellers() throws SQLException {
+        //given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER=FALSE";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER=TRUE";
+        //when
+        Statement statement2 = dbManager.getConnection().createStatement();
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement2.execute(sqlProcedureCall);
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        //then
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertEquals(1, howMany);
+        rs.close();
+        statement.close();
+        statement2.close();
+    }
 }
