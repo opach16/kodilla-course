32f5b0e50e9006929b69cfa4c54602d6b701d56d
Konrad Opach <opach16@outlook.com>
2024-12-08 17:34:31 +0100

    29.3 Task deletion from CrudApp after the test

--- kodilla-testing2/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
+++ kodilla-testing2/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
@@ -37,6 +37,7 @@ public class CrudAppTestSuite {
         String taskName = createCrudAppTestTask();
         sendTestTaskToTrello(taskName);
         assertTrue(checkTaskExistsInTrello(taskName));
+        deleteCrudAppTestTask(taskName);
     }
 
     private void sendTestTaskToTrello(String taskName) throws InterruptedException {
@@ -65,17 +66,19 @@ public class CrudAppTestSuite {
         final String XPATH_TASK_NAME = "/html/body/main/section[1]/form/fieldset[1]/input";
         final String XPATH_TASK_CONTENT = "/html/body/main/section[1]/form/fieldset[2]/textarea";
         final String XPATH_ADD_BUTTON = "/html/body/main/section[1]/form/fieldset[3]/button";
-        String taskName = "Task number " + generator.nextInt(100000);            // [1]
-        String taskContent = taskName + " content";                              // [2]
+        String taskName = "Task number " + generator.nextInt(100000);
+        String taskContent = taskName + " content";
+
+        Thread.sleep(2000);
 
         WebElement name = driver.findElement(By.xpath(XPATH_TASK_NAME));
-        name.sendKeys(taskName);                                                 // [3]
+        name.sendKeys(taskName);
 
         WebElement content = driver.findElement(By.xpath(XPATH_TASK_CONTENT));
-        content.sendKeys(taskContent);                                           // [4]
+        content.sendKeys(taskContent);
 
-        WebElement addButton = driver.findElement(By.xpath(XPATH_ADD_BUTTON));   // [5]
-        addButton.click();                                                       // [6]
+        WebElement addButton = driver.findElement(By.xpath(XPATH_ADD_BUTTON));
+        addButton.click();
         Thread.sleep(5000);
 
         return taskName;
@@ -87,7 +90,7 @@ public class CrudAppTestSuite {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get(TRELLO_URL);
 
-        Thread.sleep(5000);
+        Thread.sleep(10000);
 
         driver.findElement(By.id("username")).sendKeys("opach16@outlook.com");
         driver.findElement(By.id("login-submit")).submit();
@@ -97,16 +100,31 @@ public class CrudAppTestSuite {
         driver.findElement(By.id("password")).sendKeys("netpuw-gugkaG-9pidza");
         driver.findElement(By.id("login-submit")).submit();
 
-        Thread.sleep(2000);
+        Thread.sleep(10000);
 
         driver.findElement(By.xpath("//a[@class='board-tile']//div[text()='Kodilla Application']")).click();
 
-        Thread.sleep(2000);
+        Thread.sleep(5000);
 
         result = driver.findElements(By.xpath("//a")).stream()
                 .anyMatch(a -> a.getText().equals(taskName));
 
         driver.close();
         return result;
+    }
+
+    private void deleteCrudAppTestTask(String taskName) throws InterruptedException {
+        final String XPATH_DATATABLE = "//section[@class=\"datatable\"]//form";
+
+        Thread.sleep(5000);
+
+        driver.findElements(By.xpath(XPATH_DATATABLE)).stream()
+                .filter(form -> !form.findElements(By.xpath(".//p[@data-task-name-paragraph]")).isEmpty())
+                .filter(form -> form.findElement(By.xpath(".//p[@data-task-name-paragraph]")).getText().equals(taskName))
+                .forEach(form -> form.findElement(By.xpath(".//button[@data-task-delete-button]")).click());
+
+        Thread.sleep(2000);
+        driver.navigate().refresh();
+        Thread.sleep(5000);
     }
 }
